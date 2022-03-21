c7a7dbc6e4b8672c907e0dfbda43f0d95affe23a
Jose Damasceno <joseafcd1@hotmail.com>
2022-03-21 15:02:04 -0300

    first commit

---
+++ src/br/com/dio/desafio/dominio/Main.java
@@ -0,0 +1,31 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Main {
+	public static void main(String[] args) {
+		Curso curso1 = new Curso();
+
+		curso1.setTitulo("Curso Java");
+		curso1.setDescricao("Descrição Curso java Poo");
+		curso1.setCargaHoraria(8);
+
+		Curso curso2 = new Curso();
+
+		curso2.setTitulo("Curso Js");
+		curso2.setDescricao("Descrição Curso js ");
+		curso2.setCargaHoraria(4);
+		
+		Mentoria mentoria = new Mentoria();
+		
+		mentoria.setTitulo("Mentoria Java");
+		mentoria.setDescricao("Descrição mentoria java");
+		mentoria.setDate(LocalDate.now());
+
+		System.out.println(curso1);
+		System.out.println(curso2);
+		System.out.println(mentoria);
+
+	}
+
+}
